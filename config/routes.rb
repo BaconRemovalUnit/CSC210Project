ee11b51e97727acc3d22fea65333d30302d84592
raymondtam <raymondtamhkknt@gmail.com>
2014-10-10 10:48:35 +0800

    Updating root
    Updating root for web visit

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   put "phone/updating/:id" => "phone#updating"
   put "phone/destoring/:id" => "phone#destoring"
   
+  root :to => 'phone#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
