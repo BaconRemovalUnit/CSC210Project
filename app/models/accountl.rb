8777c66e63b51c02ae51f93f9fea47460e16208b
BaconRemovalUnit <suizsq@qq.com>
2014-10-23 17:18:03 -0400

    Changes user.rb
    added validation to email, removed accountl.rb

--- app/models/accountl.rb
+++
@@ -1,4 +0,0 @@
-class Accountl < ActiveRecord::Base
-	validates :_id, :password, :email, presence: true
-	validates_uniqueness_of :_id
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     validates :_id, :password, :email, presence: true
     validates_uniqueness_of :_id
+    validates_uniqueness_of :email
 end
