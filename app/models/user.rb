fa5af45fe451e576e863bcf6d159c8d2d4fc2aed
BaconRemovalUnit <suizsq@qq.com>
2014-12-10 14:05:21 -0500

    Minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     validates :_id, :password, :email, presence: true
+    validates_format_of :email, :with => /@/
     validates_uniqueness_of :_id
     validates_uniqueness_of :email
     
