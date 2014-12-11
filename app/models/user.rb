1be93615fa406246f32bff13ecd13a60bf082962
raymondtam <raymondtamhkknt@gmail.com>
2014-12-12 05:19:24 +0800

    Revert "Update user.rb"
    This reverts commit 2b1151818ea3bbbb7d96591cbb4c7711f9cf5b12.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     validates :_id, :password, :email, presence: true
-    validates_format_of :email, :with => \A\S+@\S.+[r][o][c][h][e][s][t][e][r]+\.[e][d][u]
+    validates_format_of :email, :with => /@/
     validates_uniqueness_of :_id
     validates_uniqueness_of :email
     
